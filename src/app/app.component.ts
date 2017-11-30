1afe97a04799c8ec531b80564f9299a45fd4bc00
eranhd <eanm22@gmail.com>
2017-11-30 08:52:44 +0200

    sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public name: string;
+  public lname: string;
+  private itemDoc: AngularFirestoreDocument<any>;
+  private col: AngularFirestoreCollection<any>;
+  public users;
+
+  public ob;
+
+  constructor(private afs: AngularFirestore) {
+    this.itemDoc = this.afs.doc("users/1");
+    this.col = this.afs.collection("users");
+    this.col.valueChanges().subscribe(res => {
+      console.log(res);
+      this.users = res;
+    });
+    this.ob = this.col.valueChanges();
+    // this.name = item.name;
+    // this.lname = item.lname;
+
+
+  }
+  send() {
+
+    this.col.add({ name: this.name, lname: this.lname }).then(res => {
+      // alert(res);
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from "angularfire2/firestore";
 
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyB-SgQ8Eq5IdXJtbd3g0sbeZtwepfAXtTM",
+    authDomain: "myapp-6faea.firebaseapp.com",
+    databaseURL: "https://myapp-6faea.firebaseio.com",
+    projectId: "myapp-6faea",
+    storageBucket: "myapp-6faea.appspot.com",
+    messagingSenderId: "992351132628"
+  }
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    FormsModule, 
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
